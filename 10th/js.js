dacaf89424fa32aa623d522929e4924619bf5ffd
FonziD1 <georgievfonzi@gmail.com>
2025-10-10 15:38:29 +0300

    10th challenge

---
+++ 10th/js.js
@@ -0,0 +1,33 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+
+//USING Async Await
+async function generateJoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+
+    const res = await fetch('https://icanhazdadjoke.com/', config);
+
+    const data = await res.json();
+
+    jokeEl.innerHTML = data.joke;
+};  
+
+/* using getch API .then() method
+function generateJoke() {
+  fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+        jokeEl.innerHTML = data.joke;
+    });
+}
+*/
+jokeBtn.addEventListener('click', generateJoke);
